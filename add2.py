39300a4a6ce9b272bf43b6d99f850a78d5b53642
HananKwok532 <hanankwok532@gmail.com>
2020-02-07 11:05:56 -0500

    Uploaded the Jenkins lesson files so far

---
+++ add2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  7 10:02:54 2020
+
+@author: hanankwok532
+"""
+
+def func(my_int: int) -> int:
+    ''' Adds 2 to an integer'''
+    return my_int + 2
---
+++ tests/test_add2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  7 10:06:11 2020
+
+@author: hanankwok532
+"""
+
+import add2
+
+def test_func():
+    '''Tests func() from add2.py'''
+    assert add2.func(2) == 4
